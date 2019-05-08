547f318c6bcb67a6898f6b92aae225a788774762
Amanda Leaders <alsleaders@gmail.com>
2019-05-07 21:32:09 -0400

    started game, buttons next

--- public/main.js
+++ public/main.js
@@ -1,7 +1,15 @@
+let myArray = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+
 const main = () => {
   if (document.querySelector('h1.hello-world')) {
     document.querySelector('h1.hello-world').textContent = 'Hello, World!'
   }
 }
 
+const compareRock = () => {
+  console.log('rock')
+  document.querySelector('.result').textContent = 'rock'
+}
+
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.rock').addEventListener('click', compareRock)
